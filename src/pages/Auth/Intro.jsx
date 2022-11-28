6425c242921c04d6f9fcdafd2f4d5127393faa0a
Hubert Ryan <hubertryanofficial@gmail.com>
2022-11-28 01:26:05 -0300

    background colors fixed

--- src/pages/Auth/Intro.jsx
+++ src/pages/Auth/Intro.jsx
@@ -7,19 +7,23 @@ import { useTailwind } from "tailwind-rn";
 const Intro = () => {
   const tw = useTailwind();
   return (
-    <View style={tw("flex-1 bg-primary")}>
+    <View style={tw("flex-1 bg-secundary")}>
       <View style={tw("flex-1 bg-white rounded-3xl")}></View>
-      <View style={tw("mx-4 mt-8 mb-10")}>
+      <View style={tw("mx-4 mt-8 mb-12")}>
         <View style={tw("flex-row items-center justify-center")}>
           <Button
             title="Criar Conta"
-            buttonStyle={tw("bg-primary")}
+            buttonStyle={tw("bg-secundary")}
             titleStyle={tw("text-white")}
             containerStyle={tw("border-2 border-white")}
           />
-          <Button title="Fazer Login" containerStyle={tw("ml-4")} />
+          <Button
+            title="Fazer Login"
+            containerStyle={tw("ml-4")}
+            titleStyle={tw("text-secundary")}
+          />
         </View>
-        <Text style={tw("text-center mt-4 text-white")}>
+        <Text style={tw("text-center mt-4 text-white font-wMedium")}>
           Esqueceu a senha? <Text style={tw("underline")}>Clique aqui</Text>
         </Text>
       </View>
